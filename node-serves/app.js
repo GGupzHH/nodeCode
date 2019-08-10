baa1e164985fd1406706120f6c1c68823d24d44a
zhaoyagu <zhao.yaguo@shinewingdigital.com>
2019-08-10 16:11:51 +0800

    link mongodb function is ok

--- node-serves/app.js
+++ node-serves/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = require('./router/router')
-
+const { insertOne } = require('./config/mongodb')
 const app = express()
 // app.use()
 
+
+console.log(insertOne)
+insertOne('user', {userName: 'zs', password: '123'}, (err, results) => {
+  console.log(results)
+})
 app.listen('8888', () => {
   console.log('It is Serve -------------')
 })
--- node-serves/config/mongodb.js
+++ node-serves/config/mongodb.js
@@ -7,8 +7,24 @@ function _connect (cb) {
       console.log(`There's something wrong with the database.`)
     } else {
       // Set up database name
-      const db = client.db('zyg')
-      cb(db)
+      const db = client.db('nodeServes')
+      cb(db, client)
     }
   })
 }
+
+/* --------------- insert One --------------- */
+/*
+  * collection 要插入的集合
+  * obj 插入的数据
+  * 操作数据库的回调
+*/
+module.exports.insertOne = function (collection, obj, cb) {
+  _connect((db, client) => {
+    db.collection(collection).insertOne(obj, (err, results) =>{
+      cb(err, results)
+    })
+    client.close()
+  })
+}
+/* --------------- insert One --------------- */
