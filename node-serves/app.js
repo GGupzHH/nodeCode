94e5ff8da3b5b47eda76a88ea31d693564867311
zhaoyagu <zhao.yaguo@shinewingdigital.com>
2019-08-10 16:34:31 +0800

    findOneById fn is ok

--- node-serves/app.js
+++ node-serves/app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const router = require('./router/router')
-const { insertOne } = require('./config/mongodb')
+const { insertOne, insertMany, findOneById } = require('./config/mongodb')
 const app = express()
 // app.use()
 
 
 console.log(insertOne)
-insertOne('user', {userName: 'zs', password: '123'}, (err, results) => {
+// insertOne('user', {userName: 'zs', password: '123'}, (err, results) => {
+//   console.log(results)
+// })
+// insertMany('user', [{name: 'zss', passworde: '321'}, {name: 'zss', passworde: '321'}], (err, results) => {
+//   console.log(results)
+// })
+findOneById('user', '5d4e8079ece99d193ca2101b', (err, results) => {
   console.log(results)
 })
 app.listen('8888', () => {
--- node-serves/config/mongodb.js
+++ node-serves/config/mongodb.js
@@ -1,4 +1,5 @@
-const mongoClient = require('mongodb').MongoClient
+const mongodb = require('mongodb')
+const mongoClient = mongodb.MongoClient
 const url = `mongodb://127.0.0.1:27017/`
 
 function _connect (cb) {
@@ -21,18 +22,28 @@ function _connect (cb) {
 */
 module.exports.insertOne = function (collection, obj, cb) {
   _connect((db, client) => {
-    db.collection(collection).insertOne(obj, (err, results) =>{
-      cb(err, results)
-    })
+    db.collection(collection).insertOne(obj, (err, results) => cb(err, results) )
     client.close()
   })
 }
 /* --------------- insert Multiple --------------- */
 module.exports.insertMany = function (collection, arr, cb) {
   _connect((db, client) => {
-    db.collection(collection).insertMany(arr, (err, results) => {
-      cb(err, results)
-    })
+    db.collection(collection).insertMany(arr, (err, results) => cb(err, results) )
+    client.close()
+  })
+}
+
+
+/* --------------- query id --------------- */
+/*
+  * collection 要插入的集合
+  * id 查找的数据
+  * 操作数据库的回调
+*/
+module.exports.findOneById = function (collection, id, cb) {
+  _connect((db, client) => {
+    db.collection(collection).findOne({ _id: mongodb.ObjectId(id) }, (err, results) => cb(err, results) )
     client.close()
   })
 }
